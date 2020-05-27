b7001beae4a452adaa9363f56a767ce79bb2c758
Restyled.io <commits@restyled.io>
2020-05-27 00:54:16 +0000

    Restyled by clang-format

--- java/LinkedLists/ListNode.java
+++ java/LinkedLists/ListNode.java
@@ -1,29 +1,26 @@
-public class ListNode{
+public class ListNode {
 
-    public int data; 
-    public ListNode next; 
-    
-    public ListNode(){
-        this(0, null); 
-    }
+  public int data;
+  public ListNode next;
 
-    public ListNode(int data){
-        this(data, null); 
-    }
+  public ListNode() { this(0, null); }
 
-    public ListNode(int data, ListNode next){
-        this.data  = data; 
-        this.next = next; 
-    }
+  public ListNode(int data) { this(data, null); }
 
-    public static void main(String[] args){
-        ListNode list = new ListNode(3, ); 
-        list.data = 3; 
-        list.next = new ListNode(); 
-        list.next.data = 4; 
-        list.next.next = new ListNode(); 
-        list.next.next.data = 5; 
+  public ListNode(int data, ListNode next) {
+    this.data = data;
+    this.next = next;
+  }
 
-        System.out.println(list.data + " " + list.next.data + " " +  list.next.next.data); 
-    }
+  public static void main(String[] args) {
+    ListNode list = new ListNode(3, );
+    list.data = 3;
+    list.next = new ListNode();
+    list.next.data = 4;
+    list.next.next = new ListNode();
+    list.next.next.data = 5;
+
+    System.out.println(list.data + " " + list.next.data + " " +
+                       list.next.next.data);
+  }
 }
--- problems/javascript/easy/palindromenumber.js
+++ problems/javascript/easy/palindromenumber.js
@@ -1,20 +1,19 @@
-function isPalindrome(){
-    //a negative int cannot be a palindrome number
-    if (x < 0) {return false;}
-        
-    //reverse the number
-    let x_copy = x ; 
-    let reversed = 0;
+function isPalindrome() {
+  // a negative int cannot be a palindrome number
+  if (x < 0) {
+    return false;
+  }
 
-    while (x_copy > 0){
-        reversed =((reversed * 10) + x_copy % 10);  
-        x_copy = parseInt(x_copy / 10); 
-    }
+  // reverse the number
+  let x_copy = x;
+  let reversed = 0;
 
-    reversed = parseInt(reverAsed)
-    
-    return (reversed === x) ? true : false; 
+  while (x_copy > 0) {
+    reversed = ((reversed * 10) + x_copy % 10);
+    x_copy = parseInt(x_copy / 10);
+  }
 
- }
+  reversed = parseInt(reverAsed)
 
- 
+  return (reversed === x) ? true : false;
+}
